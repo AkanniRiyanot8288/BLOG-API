3755ffe68d9bef5b67b3e4de847d12743ee07b80
AkanniRiyanot8288 <temmyrihan@gmail.come>
2024-09-03 06:07:45 -0700

    update blog

--- Controller/commentCtrl.js
+++ Controller/commentCtrl.js
@@ -1,6 +1,6 @@
 const Comment = require("../Model/Comment/Comment");
 const Post = require("../Model/Post/Post");
-const User = require("../model/User/User");
+const User = require("../Model/User/User");
 const appErr = require("../utils/appErr");
 
 // create comment
